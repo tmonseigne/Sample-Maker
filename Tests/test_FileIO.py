6f8bccbb445079339ff091fc74b019ddaf69c480
Thibaut <monseignethibaut@gmail.com>
2024-11-13 11:36:52 +0100

    ✅ Add Test for normalized

--- Tests/test_FileIO.py
+++ Tests/test_FileIO.py
@@ -39,7 +39,8 @@ def test_save_sample_as_png():
 	Test de la fonction save_sample_as_png.
 	"""
 	save_sample_as_png(ref, f"{OUTPUT_DIR}/test_save_sample.png", 0)
-	save_sample_as_png(np.zeros((256, 256)), f"{OUTPUT_DIR}/test_save_sample_black.png")
+	save_sample_as_png(ref, f"{OUTPUT_DIR}/test_save_sample_normalized.png", 100)
+	save_sample_as_png(np.zeros((256, 256)).astype(np.float32), f"{OUTPUT_DIR}/test_save_sample_black.png")
 
 
 ##################################################
@@ -48,8 +49,12 @@ def test_open_png_as_sample():
 	Test de la fonction open_png_as_sample.
 	"""
 	sample = open_png_as_sample(f"{OUTPUT_DIR}/test_save_sample.png")
-	new_ref = ref.astype(np.uint8).astype(np.float32)  # cast forcé car les pixel ont des valeurs entières
+	new_ref = ref.astype(np.uint8).astype(np.float32)  # cast forcé car les pixels ont des valeurs entières.
 	assert np.allclose(new_ref, sample, atol=1e-5), "L'échantillon devrait correspondre à la référence avec une tolérance d'erreur."
+
+	sample = open_png_as_sample(f"{OUTPUT_DIR}/test_save_sample_normalized.png")
+	assert np.allclose(new_ref, sample, atol=1), "L'échantillon devrait correspondre à la référence avec une tolérance d'erreur."
+	# La tolérance est de 1 degré d'intensité, car au hasard du bruit la plage de la ref pourrait être différente de [0;255] dans de très rares cas.
 
 	sample = open_png_as_sample(f"{OUTPUT_DIR}/test_save_sample_black.png")
 	assert np.allclose(sample, 0.0), "L'échantillon devrait être entièrement noir."
