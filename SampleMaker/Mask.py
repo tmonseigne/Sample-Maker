4881bf143fc0890d7206349faab4c54d98e7fe58
Thibaut <thibaut.monseigne@u-bordeaux.fr>
2024-11-19 18:11:20 +0100

    ✅ Updat Mask Tests (and members)

--- SampleMaker/Mask.py
+++ SampleMaker/Mask.py
@@ -23,9 +23,9 @@ class Mask:
 		- **size (int)** : Taille du masque.
 		- **pattern (Pattern)** : Motif à utiliser pour générer le masque.
 	"""
-	size: int = 256
-	pattern: Pattern = field(default_factory=Pattern)
-	mask: NDArray[np.bool_] = field(init=False)
+	_size: int =  field(default=256, init=True, repr=False)
+	_pattern: Pattern =  field(default_factory=Pattern, init=True, repr=False)
+	mask: NDArray[np.bool_] = field(init=False, repr=False)
 
 	# ==================================================
 	# region Initialization / Setter
@@ -39,23 +39,45 @@ class Mask:
 		self._generate()
 
 	##################################################
-	def set_size(self, size: int):
+	@property
+	def size(self) -> int:
 		"""
-		Change la taille du masque et le (re)génère.
+		Getter pour la taille du masque.
 
-		:param size: Nouvelle taille du masque.
+		:return: La taille actuelle du masque.
 		"""
-		self.size = size
+		return self._size
+
+	##################################################
+	@size.setter
+	def size(self, value: int):
+		"""
+		Setter pour la taille du masque et le (re)génère.
+
+		:param value: Nouvelle taille du masque.
+		"""
+		self._size = value
 		self._generate()
 
 	##################################################
-	def set_pattern(self, pattern: Pattern):
+	@property
+	def pattern(self) -> Pattern:
 		"""
-		Change le motif du masque et le (re)génère.
+		Getter pour le motif du masque.
+
+		:return: Le motif actuel du masque.
+		"""
+		return self._pattern
+
+	##################################################
+	@pattern.setter
+	def pattern(self, pattern: Pattern):
+		"""
+		Setter pour le motif du masque et le (re)génère.
 
 		:param pattern: Nouveau motif du masque.
 		"""
-		self.pattern = pattern
+		self._pattern = pattern
 		self._generate()
 
 	# ==================================================
@@ -71,27 +93,27 @@ class Mask:
 		Génère un masque
 		"""
 		# Création de l'image selon le motif
-		if self.pattern.pattern == PatternType.STRIPES: self._stripes_mask()
-		elif self.pattern.pattern == PatternType.SQUARES: self._squares_mask()
-		elif self.pattern.pattern == PatternType.SUN: self._sun_mask()
-		elif self.pattern.pattern == PatternType.EXISTING_IMAGE: self.open(self.pattern.options.path)
-		else: self.mask = np.full((self.size, self.size), True, dtype=bool)
+		if self._pattern.pattern == PatternType.STRIPES: self._stripes_mask()
+		elif self._pattern.pattern == PatternType.SQUARES: self._squares_mask()
+		elif self._pattern.pattern == PatternType.SUN: self._sun_mask()
+		elif self._pattern.pattern == PatternType.EXISTING_IMAGE: self.open(self._pattern.options.path)
+		else: self.mask = np.full((self._size, self._size), True, dtype=bool)
 
 	##################################################
 	def _stripes_mask(self):
 		"""
 		Génération d'un masque avec un motif de bandes.
 		"""
-		self.mask = np.full((self.size, self.size), False, dtype=bool)				 # Masque "noir" par défaut
-		limits = [float(x) for x in self.pattern.options.lengths for _ in range(2)]  # Dupliquer chaque élément (bande noire et blanche de même taille)
-		if self.pattern.options.mirror: limits.extend([1] + limits[::-1])			 # Ajout du miroir
-		cumulative_limits = list(accumulate(limits))								 # Les limites sont cumulées pour avoir leur position par rapport à 0.
-		ratio = float(self.size) / cumulative_limits[-1]							 # Calcul du ratio pixel / longueur
-		pixel_limits = [0] + [int(x * ratio) for x in cumulative_limits]			 # Conversion des positions en pixel
-		for i in range(0, len(pixel_limits) - 1, 2):								 # Parcours des Bandes Blanches
-			start, end = pixel_limits[i], pixel_limits[i + 1]						 # Définition des limites en pixel
+		self.mask = np.full((self._size, self._size), False, dtype=bool)			  # Masque "noir" par défaut
+		limits = [float(x) for x in self._pattern.options.lengths for _ in range(2)]  # Dupliquer chaque élément (bande noire et blanche de même taille)
+		if self._pattern.options.mirror: limits.extend([1] + limits[::-1])			  # Ajout du miroir
+		cumulative_limits = list(accumulate(limits))								  # Les limites sont cumulées pour avoir leur position par rapport à 0.
+		ratio = float(self._size) / cumulative_limits[-1]							  # Calcul du ratio pixel / longueur
+		pixel_limits = [0] + [int(x * ratio) for x in cumulative_limits]			  # Conversion des positions en pixel
+		for i in range(0, len(pixel_limits) - 1, 2):								  # Parcours des Bandes Blanches
+			start, end = pixel_limits[i], pixel_limits[i + 1]						  # Définition des limites en pixel
 			# Les X sont les colonnes et les Y les lignes dans un tableau donc attention aux indices.
-			if self.pattern.options.orientation: self.mask[:, start:end] = True
+			if self._pattern.options.orientation: self.mask[:, start:end] = True
 			else:   self.mask[start:end, :] = True
 
 	##################################################
@@ -99,17 +121,17 @@ class Mask:
 		"""
 		Génération d'un masque avec un motif de carrés.
 		"""
-		self.mask = np.full((self.size, self.size), False, dtype=bool)  # Masque "noir" par défaut
+		self.mask = np.full((self._size, self._size), False, dtype=bool)  # Masque "noir" par défaut
 
-		s = self.pattern.options.size  # Taille de chaque carré blanc
-		if s * 2 > self.size:		   # Si on ne peut même pas placer un carré, le masque reste noir
+		s = self._pattern.options.size  # Taille de chaque carré blanc
+		if s * 2 > self._size:		    # Si on ne peut même pas placer un carré, le masque reste noir
 			print_warning("La taille est trop grande. Masque blanc généré.")
-			self.mask = ~self.mask	   # Transformation en masque blanc
+			self.mask = ~self.mask	    # Transformation en masque blanc
 			return
 
-		n = (self.size - s) // (s * 2) + 1  # Calcul du nombre de carrés dans chaque direction (+1 pour maximiser le nombre de carrés)
+		n = (self._size - s) // (s * 2) + 1  # Calcul du nombre de carrés dans chaque direction (+1 pour maximiser le nombre de carrés)
 		# Remplissage des carrés dans le masque
-		start = (self.size - (n * (s * 2) - s)) // 2  # Calcul de la position du premier carré
+		start = (self._size - (n * (s * 2) - s)) // 2  # Calcul de la position du premier carré
 		for i in range(n):
 			for j in range(n):
 				x_start = start + i * (s * 2)
@@ -121,21 +143,21 @@ class Mask:
 		"""
 		Génération d'un masque avec un motif en forme de soleil.
 		"""
-		self.mask = np.full((self.size, self.size), False, dtype=bool)  # Masque "noir" par défaut
+		self.mask = np.full((self._size, self._size), False, dtype=bool)  # Masque "noir" par défaut
 
-		r = self.pattern.options.ray_count
+		r = self._pattern.options.ray_count
 		if not (r & r - 1) == 0:											# Vérifie que rays est une puissance de 2.
 			print_warning("Le nombre de rayons est introuvable ou manquant dans les options. Masque blanc généré.")
 			self.mask = ~self.mask											# Transformation en masque blanc
 			return
 
-		center = self.size // 2												# Centre de l'image
+		center = self._size // 2											# Centre de l'image
 		n_segments = r * 2													# Nombre de segments
 		angle_per_segment = 2 * math.pi / n_segments						# Calcul de l'angle par segment (en radians)
 
 		# Remplissage du masque
-		for x in range(self.size):
-			for y in range(self.size):
+		for x in range(self._size):
+			for y in range(self._size):
 				dx, dy = x - center, y - center								# Coordonnées par rapport au centre
 				angle = (math.atan2(dy, dx) + 2 * math.pi) % (2 * math.pi)  # Calcul de l'angle en radians par rapport au centre
 				segment = int(angle // angle_per_segment)					# Déterminer le segment dans lequel le point se situe
@@ -155,7 +177,7 @@ class Mask:
 
 		:return: la taille et la liste des options.
 		"""
-		return f"Size: {self.size}, {self.pattern}"
+		return f"Size: {self._size}, {self._pattern}"
 
 	##################################################
 	def __str__(self) -> str: return self.tostring()
@@ -174,13 +196,13 @@ class Mask:
 		Ouvre un fichier PNG et le transforme en masque de booléen
 		:param filename: Nom du fichier à ouvrir
 		"""
-		self.pattern = Pattern.from_pattern(PatternType.EXISTING_IMAGE, {"path": filename})
-		if not os.path.isfile(self.pattern.options.path):
-			print_warning(f"Aucun fichier spécifié ou le fichier est introuvable. Masque blanc de taille {self.size} généré.")
-			self.mask = np.full((self.size, self.size), True, dtype=bool)
+		self._pattern = Pattern.from_pattern(PatternType.EXISTING_IMAGE, {"path": filename})
+		if not os.path.isfile(self._pattern.options.path):
+			print_warning(f"Aucun fichier spécifié ou le fichier est introuvable. Masque blanc de taille {self._size} généré.")
+			self.mask = np.full((self._size, self._size), True, dtype=bool)
 		else:
 			self.mask = open_png_as_boolean_mask(filename)
-			self.size = self.mask.shape[0]
+			self._size = self.mask.shape[0]
 
 # ==================================================
 # endregion IO
--- Tests/test_Mask.py
+++ Tests/test_Mask.py
@@ -11,6 +11,20 @@ from SampleMaker.Pattern import Pattern, PatternType
 
 INPUT_DIR = Path(__file__).parent / "Input"
 
+
+##################################################
+def test_mask():
+	""" Test basique de la classe"""
+	mask = Mask()
+	new_size = 128
+	new_pattern = Pattern.from_pattern(PatternType.STRIPES)
+	mask.size = new_size
+	assert mask.size == 128, "Le masque n'a pas la taille attendue."
+	mask.pattern = new_pattern
+	assert mask.pattern == new_pattern, "Le masque n'a pas la taille attendue."
+	print(f"\n{mask}")
+
+
 ##################################################
 def test_generate_mask_stripes():
 	"""
@@ -196,7 +210,7 @@ def test_generate_mask_existing_image():
 	Vérifie que le masque est de type booléen (la taille en entrée est théorique, car elle dépend du fichier en entrée).
 	"""
 	options = {"path": f"{INPUT_DIR}/PALM_ref.png"}
-	mask = Mask(pattern=Pattern.from_pattern(PatternType.EXISTING_IMAGE, options))
+	mask = Mask(_pattern=Pattern.from_pattern(PatternType.EXISTING_IMAGE, options))
 
 	assert mask.mask.dtype == bool, "Le masque devrait être de type booléen."
 	assert np.any(mask.mask), "Le masque devrait contenir des valeurs True."
@@ -210,7 +224,7 @@ def test_existing_mask_options_bad_filename():
 	Vérifie que le masque est de la bonne taille, de type booléen et est entièrement blanc.
 	"""
 	options = {"path": f"{INPUT_DIR}/badfile.png"}
-	mask = Mask(pattern=Pattern.from_pattern(PatternType.EXISTING_IMAGE, options))
+	mask = Mask(_pattern=Pattern.from_pattern(PatternType.EXISTING_IMAGE, options))
 
 	assert mask.mask.shape == (256, 256), "Le masque n'a pas la taille attendue."
 	assert mask.mask.dtype == bool, "Le masque devrait être de type booléen."
