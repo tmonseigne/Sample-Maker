c47b143871a7e6d28a3568f26a680c727cca9627
Thibaut <thibaut.monseigne@u-bordeaux.fr>
2024-12-02 11:15:08 +0100

    :white_check_mark: Add Utils Tests

--- Tests/test_Tools_Utils.py
+++ Tests/test_Tools_Utils.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import numpy as np
 
 from SampleMaker.Tools.FileIO import open_png_as_sample, save_sample_as_png
-from SampleMaker.Tools.Utils import add_extension, add_grid, print_error, print_warning
+from SampleMaker.Tools.Utils import add_extension, add_grid, add_suffix, get_timestamp_for_files, print_error, print_warning
 
 INPUT_DIR = Path(__file__).parent / "Input"
 OUTPUT_DIR = Path(__file__).parent / "Output"
@@ -14,14 +14,9 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)  # Créer le dossier de sorties (la premi
 
 ##################################################
 def test_add_extension():
-	"""
-	Test de la fonction add extension.
-	"""
+	""" Test de la fonction add extension. """
 
 	filename = "filename.extension"
-
-	res = add_extension(filename, "new")
-	assert res == "filename.extension.new", f"Le nom de fichier ne correspond pas"
 
 	res = add_extension(filename, "new")
 	assert res == "filename.extension.new", f"Le nom de fichier ne correspond pas"
@@ -38,30 +33,45 @@ def test_add_extension():
 	res = add_extension(filename, "new")
 	assert res == "file/name/extension.new", f"Le nom de fichier ne correspond pas"
 
+##################################################
+def test_add_suffix():
+	""" Test de la fonction add extension. """
+
+	filename = "filename.extension"
+	suffix = "_suffix"
+	res = add_suffix(filename, suffix)
+	assert res == "filename_suffix.extension", f"Le nom de fichier ne correspond pas"
+	filename = "filename"
+	res = add_suffix(filename, suffix)
+	assert res == "filename_suffix", f"Le nom de fichier ne correspond pas"
+
+
+##################################################
+def test_get_timestamp_for_files():
+	""" Test de la fonction get timestamp for files. """
+
+	res = get_timestamp_for_files(True)
+	print(res)
+	res = get_timestamp_for_files(False)
+	print(res)
 
 ##################################################
 def test_print_error():
-	"""
-	Test de la fonction print error.
-	"""
+	""" Test de la fonction print error. """
 
 	print_error("Message d'erreur"), "L'affichage n'a pas pu être effectué"
 
 
 ##################################################
 def test_print_warning():
-	"""
-	Test de la fonction print warning.
-	"""
+	""" Test de la fonction print warning. """
 
 	print_warning("Message d'avertissement"), "L'affichage n'a pas pu être effectué"
 
 
 ##################################################
 def test_add_grid():
-	"""
-	Test de la fonction add grid.
-	"""
+	""" Test de la fonction add grid. """
 
 	size = 256
 	image = np.zeros((size, size), dtype=np.float32)  # Image noire
